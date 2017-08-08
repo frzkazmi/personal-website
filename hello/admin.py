c5ef82a90680266527039378857d9eba13b5c8d4
Mohd Joun <mohammad.joun@nviz.com>
2017-08-08 22:48:24 +0530

    override admin view for Blog model

--- hello/admin.py
+++ hello/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
 from .models import *
 
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "date"]
+    list_display_links = ["title"]
+    #list_editable = ["title"]
+    list_filter = ["date", "title"]
+
+    search_fields = ["title", "content", "keywords"]
+    class Meta:
+        model = Blog
+
+
+
 
 admin.site.register(Person)
+admin.site.register(Blog, PostModelAdmin)
 admin.site.register(PersonalProject)
 admin.site.register(Company)
 admin.site.register(CompanyProject)
 admin.site.register(Education)
 admin.site.register(TechnicalSkill)
 admin.site.register(Skills)
-admin.site.register(Blog)
+#admin.site.register(Blog)
 admin.site.register(TagsforBlog)
 admin.site.register(Comment)
 admin.site.register(Subscriber)
